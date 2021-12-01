ba095c3935d74e5902bd4ca9ee2cdd3a4f985609
GeorgeFlorian <georgeflorian12@gmail.com>
2021-12-01 21:57:26 +0200

    Finished Project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const KEY = 'f5db5993-5374-4cd9-8c39-b1bb90bb73a1';
+export const MODAL_CLOSE_SEC = 2.5;
